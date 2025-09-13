200e9292543ab0f2d06afa26dcb0c32629144fd2
Gustavo Freitas <freitasgustavos@hotmail.com>
2025-09-13 19:28:57 -0300

    Footer

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Layout } from "antd";
+import { Flex, Layout } from "antd";
+import { GithubOutlined } from '@ant-design/icons';
 import { AppHeader } from "./AppHeader";
 import { ClientOnly } from "../ui/ClientOnly";
+import Link from "next/link";
 
 interface AppLayoutProps {
     children: React.ReactNode;
@@ -18,6 +20,14 @@ export const AppLayout = ({ children }: AppLayoutProps) => {
                 <Content style={{ padding: 24 }}>
                     {children}
                 </Content>
+                <Layout.Footer style={{ padding: 16 }}>
+                    <Flex justify="center" align="center" gap={8}>
+                        © {new Date().getFullYear()} By Gustavo Freitas
+                        <Link href="https://github.com/freitasgustavos/step-by-step" target="_blank">
+                            <GithubOutlined style={{ fontSize: 18 }} />
+                        </Link>
+                    </Flex>
+                </Layout.Footer>
             </Layout>
         </ClientOnly>
     );
